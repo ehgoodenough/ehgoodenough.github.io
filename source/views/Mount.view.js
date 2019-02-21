3eb9bd3b3e2a6cd3dc55170c7a8e545efdc3888e
Andrew McPherson <ehgoodenough@gmail.com>
2019-02-21 14:13:38 -0800

    Added a bunch of other games.

--- source/views/Mount.view.js
+++ source/views/Mount.view.js
@@ -6,9 +6,21 @@ const portfolio = {
     "games": [
         {
             "title": "Else Return Home",
-            "image": require("images/else-return-home-2.png"),
+            "image": require("images/else-return-home.gif"),
             "link": "https://ackleyrc.itch.io/else-return-home",
             "jam": "Global Game Jam 2019",
+        },
+        {
+            "title": "Twitch Chat Lobby",
+            "image": require("images/twitch-chat-lobby.png"),
+            "link": "https://globalgamejam.org/2018/games/twitch-chat-lobby",
+            "jam": "Global Game Jam 2018"
+        },
+        {
+            "title": "Stake your Acclaim",
+            "image": require("images/stake-your-acclaim.png"),
+            "link": "https://ldjam.com/events/ludum-dare/40/stake-your-acclaim",
+            "jam": "Ludum Dare 40",
         },
         {
             "title": "Heartslingers",
@@ -23,16 +35,90 @@ const portfolio = {
             "jam": "Game By Its Cover Game 2017",
         },
         {
+            "title": "Plug-n-Play",
+            "image": require("images/plug-n-play.gif"),
+            "link": "https://ldjam.com/events/ludum-dare/39/plug-and-play",
+            "jam": "Ludum Dare 39",
+        },
+        {
             "title": "Fool's Gold",
-            "image": require("images/fools-gold.gif"),
+            "image": require("images/fools-gold-2.gif"),
             "link": "https://ehgoodenough.itch.io/fools-gold",
             "jam": "Seattle Indies Jam 2017",
+        },
+        {
+            "title": "WootWare",
+            "image": require("images/wootware.gif"),
+            "link": "https://ehgoodenough.github.io/microgames",
+            "jam": "Woot Jam 2017",
         },
         {
             "title": "Punch-a-Bunch",
             "image": require("images/punch-a-bunch.gif"),
             "link": "https://globalgamejam.org/2017/games/shining-dragon-showdown-sunset-against-shadows",
             "jam": "Global Game Jam 2017",
+        },
+        {
+            "title": "Salt Pile",
+            "image": require("images/salt-pile.gif"),
+            "link": "http://ludumdare.com/compo/ludum-dare-37/?action=preview&uid=41870",
+            "jam": "Ludum Dare 37",
+        },
+        {
+            "title": "Fracture",
+            "image": require("images/fracture.gif"),
+            "link": "https://globalgamejam.org/2016/games/fracture",
+            "jam": "Global Game Jam 2016",
+        },
+        {
+            "title": "Overclock.exe",
+            "image": require("images/overclock.gif"),
+            "link": "http://js13kgames.com/entries/overclockexe",
+            "jam": "JS13K 2016",
+        },
+        {
+            "title": "Stick Them With the Pointy End",
+            "image": require("images/stick-em-1.png"),
+            "link": "http://ludumdare.com/compo/ludum-dare-36/?action=preview&uid=41870",
+            "jam": "Ludum Dare 36",
+        },
+        {
+            "jam": "Social Justice Jam 2016",
+            "image": require("images/nodapl.png"),
+            "link": "https://ehgoodenough.github.io/nodapl",
+        },
+        {
+            "title": "Stack",
+            "image": require("images/stack.gif"),
+            "jam": "A9VS Hackathon 2016",
+        },
+        {
+            "title": "Wearwolf",
+            "image": require("images/wearwolf.gif"),
+            "link": "http://ludumdare.com/compo/ludum-dare-35/?action=preview&uid=41870",
+            "jam": "Ludum Dare 35",
+        },
+        {
+            "image": require("images/roguelike.gif"),
+            "jam": "7DRL 2016",
+        },
+        {
+            "title": "Snake-on-a-Plane",
+            "image": require("images/snake-on-a-plane.gif"),
+            "link": "http://ehgoodenough.github.io/snake-on-a-plane/v0.0.2/",
+            "jam": "GDC Plane Jam 2016",
+        },
+        {
+            "title": "Sproutsong",
+            "image": require("images/sproutsong.gif"),
+            "link": "http://ludumdare.com/compo/ludum-dare-34/?action=preview&uid=41870",
+            "jam": "Ludum Dare 34",
+        },
+        {
+            "title": "Papercut",
+            "image": require("images/papercut.gif"),
+            "link": "https://github.com/ehgoodenough/papercut/releases",
+            "jam": "Playfab Jam 2015",
         },
         {
             "title": "Bit Fighter",
@@ -53,10 +139,27 @@ const portfolio = {
             "jam": "Global Game Jam 2015",
         },
         {
+            "title": "Jumpdude",
+            "image": require("images/jumpdude-1.gif"),
+            "jam": "Yeah Good Enough Jam 2014",
+        },
+        {
             "title": "Whoami",
             "image": require("images/whoami.gif"),
             "link": "http://ehgoodenough.github.io/whoami/v0.1.9",
             "jam": "ChaDevs Jam 2014",
+        },
+        {
+            "title": "Inverse Worlds",
+            "image": require("images/inverse-worlds.png"),
+            "link": "http://ludumdare.com/compo/ludum-dare-30/?action=preview&uid=41870",
+            "jam": "Ludum Dare 30",
+        },
+        {
+            "title": "Nothingness",
+            "image": require("images/nothingness.png"),
+            "link": "http://mocsarcade.github.io/nothingness/",
+            "jam": "UTC CPSC4910",
         },
         {
             "title": "OpOb",
